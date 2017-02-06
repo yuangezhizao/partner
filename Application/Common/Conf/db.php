9d09030eb6912fa77bbddc1732cdca2a2015a720
侯以鹏 <1337033340@qq.com>
2017-02-06 12:07:21 +0800

    Update db.php

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -4,10 +4,10 @@
 return array(
 	//'配置项'=>'配置值'
 	'DB_TYPE'               =>  'Mysql',     // 数据库类型
-	'DB_HOST'               =>  'rm-bp1k7d5393250snh0.mysql.rds.aliyuncs.com', // 服务器地址
-	'DB_NAME'               =>  'koudai',          // 数据库名
-	'DB_USER'               =>  'koudai',      // 用户名
-	'DB_PWD'                =>  'vmW35XnAv8h2xCTD',          // 密码
+	'DB_HOST'               =>  '', // 服务器地址
+	'DB_NAME'               =>  '',          // 数据库名
+	'DB_USER'               =>  '',      // 用户名
+	'DB_PWD'                =>  '',          // 密码
 	'DB_PORT'               =>  '3306',        // 端口
 	// 'DB_PREFIX'             =>  'kdgx_',    // 数据库表前缀
 	'DB_FIELDTYPE_CHECK'    =>  false,       // 是否进行字段类型检查
